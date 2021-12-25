6c50f93afdbaba84d3e3542eb53438f46dc81ad5
benny-n <benick12@gmail.com>
2021-12-25 18:59:29 +0200

    change database design

--- backend/init_db.sql
+++ backend/init_db.sql
@@ -1,15 +1,26 @@
+CREATE TABLE poll(
+    uid BIGINT PRIMARY KEY,
+    telegram_id BIGINT
+);
+CREATE TABLE poll_props(
+    poll_uid BIGINT,
+    question VARCHAR(1023),
+    -- TODO add properties (is_anonymous, quiz or regular)
+    FOREIGN KEY(poll_uid) REFERENCES poll(uid) ON DELETE CASCADE
+);
+CREATE TABLE poll_answer(
+    poll_uid BIGINT,
+    answer VARCHAR(255),
+    PRIMARY KEY(poll_uid, answer)
+);
 CREATE TABLE "user"(
-    chat_id DOUBLE PRIMARY KEY,
+    chat_id BIGINT PRIMARY KEY,
     username VARCHAR(255)
 );
-CREATE TABLE poll(
-    poll_id DOUBLE PRIMARY KEY,
-    question VARCHAR(1023)
-);
-CREATE TABLE response(
-    chat_id DOUBLE,
-    poll_id DOUBLE,
+CREATE TABLE user_response(
+    chat_id BIGINT,
+    poll_uid BIGINT,
     answer VARCHAR(255),
-    FOREIGN KEY(chat_id) REFERENCES "user"(chat_id) ON DELETE CASCADE,
-    FOREIGN KEY(poll_id) REFERENCES poll(poll_id) ON DELETE CASCADE
+    FOREIGN KEY(poll_uid, answer) REFERENCES poll_answer(poll_uid, answer) ON DELETE CASCADE,
+    FOREIGN KEY(chat_id) REFERENCES "user"(chat_id) ON DELETE CASCADE
 );
