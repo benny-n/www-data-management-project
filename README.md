89610aa685e58d98d9ce622806a53a01ecac191b
benny-n <benick12@gmail.com>
2022-01-13 01:13:52 +0200

    add readme, add frontend env vars generation to run_project script

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### To run the project, open the terminal and run the following commands:
+
+1. ```conda env create -f environment.yml ```
+2. ```cd frontend  ```
+3. ```npm install```
+4. ```cd ..```
+5. ```python run_project.py ```
+
+*** To set your postgres db settings, edit the variables in backend/config.py 
--- run_project.py
+++ run_project.py
@@ -1,11 +1,15 @@
 import os
 import subprocess
+import backend.config as config
 
 
 def run_project():
     os.putenv('WERKZEUG_RUN_MAIN', 'true')
     os.popen('conda activate WWWDataManagement && python backend/bot.py')
     os.popen('conda activate WWWDataManagement && python backend/app.py')
+    with open("frontend/.env", 'w') as file:
+        file.write(f"REACT_APP_API_URL=http://localhost:{config.port}\n")
+        file.write(f"REACT_APP_SECRET={config.secret}\n")
     subprocess.check_call('npm start --prefix frontend/', shell=True)
 
 
