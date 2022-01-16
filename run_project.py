a2c8135ba1d582a09b22a3cec285279c1eb842e2
benny-n <benick12@gmail.com>
2022-01-16 20:10:03 +0200

    fix run_project and environment

--- run_project.py
+++ run_project.py
@@ -1,16 +1,31 @@
 import os
 import subprocess
+import time
 import backend.config as config
 
 
 def run_project():
     os.putenv('WERKZEUG_RUN_MAIN', 'true')
-    os.popen('conda activate WWWDataManagement && python backend/bot.py')
-    os.popen('conda activate WWWDataManagement && python backend/app.py')
-    with open("frontend/.env", 'w') as file:
-        file.write(f"REACT_APP_API_URL=http://localhost:{config.port}\n")
-        file.write(f"REACT_APP_SECRET={config.secret}\n")
-    subprocess.check_call('npm start --prefix frontend/', shell=True)
+    bot = None
+    app = None
+    npm = None
+    try:
+        bot = subprocess.Popen('conda activate WWWDataManagement && python backend/bot.py', shell=True)
+        app = subprocess.Popen('conda activate WWWDataManagement && python backend/app.py', shell=True)
+        with open("frontend/.env", 'w') as file:
+            file.write(f"REACT_APP_API_URL=http://localhost:{config.port}\n")
+            file.write(f"REACT_APP_SECRET={config.secret}\n")
+        npm = subprocess.Popen(f'set PORT={config.react_port} && npm start --prefix frontend/', shell=True)
+        while True:
+            bot.poll()
+            app.poll()
+            npm.poll()
+            time.sleep(10)
+
+    except KeyboardInterrupt:
+        bot.kill()
+        app.kill()
+        npm.kill()
 
 
 if __name__ == '__main__':
